c77a75ca48455bc0f72eb9ce5f82219116654db0
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-03-06 15:45:36 +0900

    [김은지]컨벤션 수정

--- products/views.py
+++ products/views.py
@@ -48,7 +48,7 @@ class DetailView(View):
                 'reviews'         : reviews_li if reviews_li else None,
             }
 
-            return JsonResponse({'result':result}, status =200)                
+            return JsonResponse({'result':result}, status=200)                
         
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status=400)
