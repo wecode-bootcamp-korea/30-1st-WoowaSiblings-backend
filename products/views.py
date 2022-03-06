384d959628530ba3899ee0101fe2be6c040095b9
DGK <rlaehdrb0923@gmail.com>
2022-03-06 11:20:22 +0900

    Modify : products/views.py

--- products/views.py
+++ products/views.py
@@ -11,13 +11,14 @@ class ProductListView(View):
             limit    = int(request.GET.get('limit', 12))
             offset   = int(request.GET.get('offset', 0))
             category = request.GET.get('category', None)
+            sorting  = request.GET.get('sort', '-created_at')
             
             q = Q()
             
             if category:
                 q &= Q(category__name=category)
 
-            products = Product.objects.filter(q)[offset:offset+limit]
+            products = Product.objects.filter(q).order_by(sorting)[offset:offset+limit]
             results  = []
             
             for product in products:
@@ -31,8 +32,8 @@ class ProductListView(View):
                         'id'              : product.id,
                         'name'            : product.name,
                         'price'           : float(product.price),
-                        'discount_rate'   : discount_rate if on_discount else None,
-                        'discount_price'  : float(product.price) * (1-discount_rate) if on_discount else None, 
+                        'discount_rate'   : discount_rate if on_discount else '',
+                        'discount_price'  : float(product.price) * (1-discount_rate) if on_discount else '', 
                         'on_discount'     : on_discount,
                         'product_option'  : product.product_option,
                         'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
