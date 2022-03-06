9d5ca4fad925cd58c6354dceab01b5f21c08e1df
DGK <rlaehdrb0923@gmail.com>
2022-03-06 10:42:58 +0900

    Modify : products/views.py

--- products/views.py
+++ products/views.py
@@ -21,21 +21,19 @@ class ProductListView(View):
             results  = []
             
             for product in products:
-                if product.on_discount:
-                    discount_rate = float(product.productsdiscountrate_set.\
-                                            get(product_id=product.id).\
-                                            discount_rate.discount_rate)/100
-                else:
-                    discount_rate = 0
-                
+                on_discount   = product.on_discount
+                discount_rate = float(product.productsdiscountrate_set.\
+                                      get(product_id=product.id).\
+                                      discount_rate.discount_rate)/100
+    
                 results.append(
                     {
                         'id'              : product.id,
                         'name'            : product.name,
                         'price'           : float(product.price),
-                        'discount_rate'   : discount_rate,
-                        'discount_price'  : float(product.price) * (1-discount_rate), 
-                        'on_discount'     : product.on_discount,
+                        'discount_rate'   : discount_rate if on_discount else None,
+                        'discount_price'  : float(product.price) * (1-discount_rate) if on_discount else None, 
+                        'on_discount'     : on_discount,
                         'product_option'  : product.product_option,
                         'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
                         'category'        : product.category.name,
