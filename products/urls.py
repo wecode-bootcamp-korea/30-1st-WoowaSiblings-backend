2bea344dc7a9fbd62dfde15b6dd4ad3c87925c82
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-03-07 14:57:59 +0900

    [김은지] path parameter, 리스트 컴프리헨션, 삼항연산자 등 적용

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from products.views import DetailView
 
 urlpatterns = [
-    path('/detail', DetailView.as_view())
+    path('/<int:product_id>', DetailView.as_view())
 ]
--- products/views.py
+++ products/views.py
@@ -1,61 +1,32 @@
 from django.http  import JsonResponse
 from django.views import View
 
-from products.models import *
+from products.models import Product
 
 class DetailView(View):
-    def get(self, request):
+    def get(self, request, product_id):
         try:
-            product = Product.objects.get(id=request.GET.get('product_id'))
+            product = Product.objects.get(id=product_id)
 
-            if product.on_discount:
-                discount_rate = float(product.productsdiscountrate_set.
-                                       get(product_id=product.id).\
-                                       discount_rate.discount_rate)/100
-            
-            if product.product_option:
-                product_options = product.producttime_set.\
-                                  filter(product_id=product.id)
-                times=[]                  
-                for product_option in product_options:
-                    times.append(product_option.time.name)
-            
-            reviews = product.review_set.filter(product_id=product.id)
-            reviews_li=[]
-            for review in reviews:
-                reviews_li.append(
+            result = {
+                'name'            : product.name,
+                'stock'           : product.stock,
+                'price'           : product.price if not product.stock == 0 else None,
+                'discount_rate'   : float(product.productsdiscountrate_set.get(product_id=product.id).discount_rate.discount_rate)/100 if product.on_discount else None,
+                'service_detail'  : product.service_detail.content,
+                'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
+                'detail_images'   : [detail_image.detail_image_url for detail_image in product.detailimage_set.all()],
+                'product_options' : [product_option.time.name for product_option in product.producttime_set.filter(product_id=product.id)] if product.product_option else None,
+                'reviews'         : [
                     {
                         'title'         : review.title,
                         'content'       : review.content,
                         'star_rating'   : review.star_rating,
                         'user'          : review.user.username,
                         'review_images' : [review_image.review_image_url for review_image in review.reviewimage_set.filter(review_id=review.id)]
-                    }
-                )
-                
-            if product.stock == 0:
-                result = {
-                    'name'            : product.name,
-                    'price'           : None,
-                    'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
-                    'detail_images'   : [detail_image.detail_image_url for detail_image in product.detailimage_set.all()],
-                    'service_detail'  : product.service_detail.content,
-                    'reviews'         : reviews_li if reviews_li else None,                
-                }
-                return JsonResponse({'result':result, 'message':'OUT_OF_STOCK'}, status=200)      
-            else:    
-                result = {
-                    'name'            : product.name,
-                    'stock'           : product.stock,
-                    'price'           : product.price,
-                    'discount_rate'   : discount_rate if product.on_discount else None,
-                    'service_detail'  : product.service_detail.content,
-                    'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
-                    'detail_images'   : [detail_image.detail_image_url for detail_image in product.detailimage_set.all()],
-                    'product_options' : times if product.product_option else None,
-                    'reviews'         : reviews_li if reviews_li else None,
-                }
-
+                    }for review in product.review_set.filter(product_id=product.id)
+                ] 
+            }
             return JsonResponse({'result':result, 'message':'SUCCESS'}, status=200)      
         
         except KeyError:
