cbf40023620fc70769690aa9392c726cc8459fd9
kimeunji <78680486+thisiseunji@users.noreply.github.com>
2022-03-07 17:37:01 +0900

    [김은지]리스트 컴프리헨션 개선

--- products/views.py
+++ products/views.py
@@ -12,20 +12,25 @@ class DetailView(View):
                 'name'            : product.name,
                 'stock'           : product.stock,
                 'price'           : product.price if not product.stock == 0 else None,
-                'discount_rate'   : float(product.productsdiscountrate_set.get(product_id=product.id).discount_rate.discount_rate)/100 if product.on_discount else None,
-                'discount_price'  : 1-float(product.productsdiscountrate_set.get(product_id=product.id).discount_rate.discount_rate)/100 if product.on_discount else None, 
+                'discount_rate'   : float(product.productsdiscountrate_set.\
+                                    get(product_id=product.id).discount_rate.discount_rate)/100\
+                                    if product.on_discount else None,
+                'discount_price'  : (1-float(product.productsdiscountrate_set.\
+                                    get(product_id=product.id).discount_rate.discount_rate)/100)*float(product.price)\
+                                    if product.on_discount else None, 
                 'service_detail'  : product.service_detail.content,
                 'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
                 'detail_images'   : [detail_image.detail_image_url for detail_image in product.detailimage_set.all()],
-                'product_options' : [product_option.time.name for product_option in product.producttime_set.filter(product_id=product.id)] if product.product_option else None,
+                'product_options' : [product_option.time.name for product_option in product.producttime_set.all()]\
+                                    if product.product_option else None,
                 'reviews'         : [
                     {
                         'title'         : review.title,
                         'content'       : review.content,
                         'star_rating'   : review.star_rating,
                         'user'          : review.user.username,
-                        'review_images' : [review_image.review_image_url for review_image in review.reviewimage_set.filter(review_id=review.id)]
-                    }for review in product.review_set.filter(product_id=product.id)
+                        'review_images' : [review_image.review_image_url for review_image in review.reviewimage_set.all()]
+                    }for review in product.review_set.all()
                 ] 
             }
             return JsonResponse({'result':result, 'message':'SUCCESS'}, status=200)      
