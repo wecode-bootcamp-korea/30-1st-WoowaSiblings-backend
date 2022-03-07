b1f4f0042e97dfbc6f558755b3dd5bb196a26a1a
DGK <rlaehdrb0923@gmail.com>
2022-03-07 16:27:56 +0900

    Modify : products/views.py

--- products/views.py
+++ products/views.py
@@ -11,39 +11,46 @@ class ProductListView(View):
             limit    = int(request.GET.get('limit', 12))
             offset   = int(request.GET.get('offset', 0))
             category = request.GET.get('category', None)
-            sorting  = request.GET.get('sort', '-created_at')
+            sorting  = request.GET.get('order_by', 'new_products')
+            
+            sorting_dict = {
+                'new_products'  : '-created_at',
+                'old_products'  : 'created_at',
+                'low_price'     : 'price',
+                'high_price'    : '-price',
+                'hot_products'  : '-likes'
+            }
             
             q = Q()
             
             if category:
                 q &= Q(category__name=category)
 
-            products = Product.objects.filter(q).order_by(sorting)[offset:offset+limit]
-            results  = []
+            products = Product.objects.filter(q).prefetch_related('like_set').\
+                       annotate(likes=Count('like')).order_by(sorting_dict[sorting])\
+                       [offset:offset+limit]
             
-            for product in products:
-                on_discount   = product.on_discount
-                discount_rate = float(product.productsdiscountrate_set.\
-                                      get(product_id=product.id).\
-                                      discount_rate.discount_rate)/100
-                likes         = product.like_set.all().aggregate(Count('product_id'))\
-                                ['product_id__count']
-    
-                results.append(
-                    {
-                        'id'              : product.id,
-                        'name'            : product.name,
-                        'price'           : float(product.price),
-                        'discount_rate'   : discount_rate if on_discount else '',
-                        'discount_price'  : float(product.price) * (1-discount_rate) if on_discount else '', 
-                        'on_discount'     : on_discount,
-                        'product_option'  : product.product_option,
-                        'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
-                        'category'        : product.category.name,
-                        'likes'           : likes
-                    }
-                )
-                
+            results = [
+                {
+                    'id'              : product.id,
+                    'name'            : product.name,
+                    'price'           : float(product.price),
+                    'discount_rate'   : float(product.productsdiscountrate_set.\
+                                             get(product_id=product.id).\
+                                             discount_rate.discount_rate)/100\
+                                             if product.on_discount else '',
+                    'discount_price'  : (1-float(product.productsdiscountrate_set.\
+                                             get(product_id=product.id).discount_rate.\
+                                             discount_rate)/100) * float(product.price)\
+                                             if product.on_discount else '',
+                    'on_discount'     : product.on_discount,
+                    'product_options' : product.product_option,
+                    'thumbnail_image' : product.thumbnailimage.thumbnail_image_url,
+                    'category'        : product.category.name,
+                    'likes'           : product.like_set.count()
+                }
+            for product in products]
+            
             return JsonResponse({'message' : 'SUCCESS', 'results' : results}, status=200)
 
         except Product.DoesNotExist:
